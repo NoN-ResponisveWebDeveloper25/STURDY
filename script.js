0605b9be8458e44691dc8eb737eb8468e19d665e
NoN Responsive WebDeveloper <gg.georgemail50a@gmail.com>
2023-06-04 03:31:54 +0530

    pagesix

--- script.js
+++ script.js
@@ -1,32 +1,48 @@
 // ----------------------LETS GO-------------------------
 
+gsap.registerPlugin(ScrollTrigger);
 
 
 
+var six=document.querySelectorAll(".pagesix>h1");
+console.log(six)
 
 
+six.forEach(function(elem){
+ gsap.to(elem,{
+  // transform:"rotateX(90deg)",
+  color:"black",
+
+  scrollTrigger:{
+    trigger:elem,
+    scroller:"body",
+    start:"top 50%",
+    end:"top 10%",
+    // markers:true,
+     scrub:2,
+  }
 
 
-// ------------------circle effect on mouse move on wapper-----------------------------
+ })
 
-var wrap=document.querySelector(".teenoverlay");
-var circle=document.querySelector(".cursor");
- 
-wrap.addEventListener("mouseenter",function(dets){
-
-circle.style.scale= 1;
-
+  
 
 })
 
-wrap.addEventListener("mouseleave",function(dets){
 
-  circle.style.scale= 0;
-  
-  
-  })
 
-  wrap.addEventListener("mousemove",function(elem){
+
+
+
+
+
+
+
+var circle=document.querySelector(".cursor");
+var main=document.querySelector(".main");
+ 
+
+  main.addEventListener("mousemove",function(elem){
     circle.style.left=elem.x+"px"
     circle.style.top=elem.y+"px"
 
@@ -34,78 +50,6 @@ wrap.addEventListener("mouseleave",function(dets){
 
 
 
-
-  
-var wraptwo=document.querySelector(".teenoverlay2");
-var circletwo=document.querySelector(".cursor2");
- 
-wraptwo.addEventListener("mouseenter",function(dets){
-
-circletwo.style.scale= 1;
-
-
-})
-
-wraptwo.addEventListener("mouseleave",function(dets){
-
-  circletwo.style.scale= 0;
-  
-  
-  })
-
-  wraptwo.addEventListener("mousemove",function(elem){
-    circletwo.style.left=elem.x+"px"
-    circletwo.style.top=elem.y+"px"
-
-})
-
-
-
-
-var wrapthree=document.querySelector(".teenoverlay3");
-var circlethree=document.querySelector(".cursor3");
- 
-wrapthree.addEventListener("mouseenter",function(dets){
-
-circlethree.style.scale= 1;
-
-
-})
-
-wrapthree.addEventListener("mouseleave",function(dets){
-
-  circlethree.style.scale= 0;
-  
-  
-  })
-
-  wrapthree.addEventListener("mousemove",function(elem){
-    circlethree.style.left=elem.x+"px"
-    circlethree.style.top=elem.y+"px"
-
-})
-
-
-
-
-
-
-
-
-
-
-
-
-window.addEventListener("scroll", function(elem){
-
-   console.log(scrollY);
-
-  console.log("hui hui");
-  
-  
-   circle.style.top=elem.scrollY+"px"
-  
-  })
 
 
 
